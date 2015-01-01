2b4cf33409614230d0934a18ef29f9de9e59d916
lbanchio <lbanchio@gmail.com>
2015-01-01 20:38:48 -0300

    Initial version added

---
+++ js/main.js
@@ -0,0 +1,32 @@
+var word = [
+    'buenísimo.',
+    'fantástico.',
+    'increíble.',
+    'asombroso.',
+    'estupendo.',
+    'genial.'];
+
+var current = 1;
+var timer = null;
+
+function nextWord() {
+  if (current < word.length) {
+    ret = word[current];
+    current = current + 1;
+    return ret;
+  }
+
+  current = 1;
+  return word[0];
+}
+
+function changeWord () {
+  $('#adj').text(nextWord());
+  $('#adj').removeClass().addClass('animated flipInX').on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){$(this).removeClass();});
+
+}
+
+$(document).on("ready",
+  function () {
+    timer = setInterval(changeWord, 3000);
+  });
